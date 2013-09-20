2c5399aed09ee576226512e9865340ead7efa6c8
IEVGEN KHVEDCHENIA <ekhvedchenya@gmail.com>
2013-09-20 23:58:08 +0300

    Added two explici mime-types for png and jpeg image types

--- src/node/node_helpers.hpp
+++ src/node/node_helpers.hpp
@@ -5,39 +5,42 @@
 #include <node_buffer.h>
 #include <v8.h>
 
-    std::string toDataUri(cv::Mat& img, const char * imageMimeType = "image/png");
+#define kImageTypePng   "image/png"
+#define kImageTypeJpeg  "image/png"
+
+std::string toDataUri(cv::Mat& img, const char * imageMimeType = kImageTypePng);
+
+struct ScopedTimer
+{
+    inline ScopedTimer()
+    {
+        m_startTime = cv::getTickCount();
+    }
     
-    struct ScopedTimer
+    inline float executionTimeMs() const
     {
-        inline ScopedTimer()
-        {
-            m_startTime = cv::getTickCount();
-        }
-        
-        inline float executionTimeMs() const
-        {
-            return (cv::getTickCount() - m_startTime) * 1000. / cv::getTickFrequency();
-        }
-        
-        int64_t m_startTime;
-    };
+        return (cv::getTickCount() - m_startTime) * 1000. / cv::getTickFrequency();
+    }
+    
+    int64_t m_startTime;
+};
 
 
-    class ObjectBuilder
-    {
-    public:
-        typedef v8::Local<v8::Object> NodeObject;
+class ObjectBuilder
+{
+public:
+    typedef v8::Local<v8::Object> NodeObject;
 
-        ObjectBuilder(NodeObject& obj);
+    ObjectBuilder(NodeObject& obj);
 
-        ObjectBuilder& Set(const char * name, int value);
-        ObjectBuilder& Set(const char * name, size_t value);
-        ObjectBuilder& Set(const char * name, float value);
-        ObjectBuilder& Set(const char * name, const char * value);
-        ObjectBuilder& Set(const char * name, const std::string& value);
+    ObjectBuilder& Set(const char * name, int value);
+    ObjectBuilder& Set(const char * name, size_t value);
+    ObjectBuilder& Set(const char * name, float value);
+    ObjectBuilder& Set(const char * name, const char * value);
+    ObjectBuilder& Set(const char * name, const std::string& value);
 
-        ObjectBuilder& Set(const char * name, const cv::Size& size);
-        
-    private:
-        NodeObject& m_object;
-    };
+    ObjectBuilder& Set(const char * name, const cv::Size& size);
+    
+private:
+    NodeObject& m_object;
+};
