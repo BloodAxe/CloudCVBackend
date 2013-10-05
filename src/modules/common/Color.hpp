e0b983b884c208896f170e0bf9a4924f276556ec
Ievgen Khvedchenia <ekhvedchenya@gmail.com>
2013-10-05 22:34:03 +0300

    Add explicitly-typed rgb color structure

---
+++ src/modules/common/Color.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <stdint.h>
+
+template <typename ValueType>
+struct rgb_color_t
+{
+	typedef ValueType value_t;
+
+	value_t r;	
+	value_t g;	
+	value_t b;	
+};
+
+typedef rgb_color_t<uint8_t> rgb8u_color_t;
+typedef rgb_color_t<uint8_t> rgb32f_color_t;
